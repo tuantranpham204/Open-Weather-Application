7d47142e7ef3f5e91df934f4dd05a93e6a50783f
tuantranpham204 <12a1tranphamtuan@gmail.com>
2025-11-19 17:14:04 +0700

    Change setup register login

--- backend/weather_api/urls.py
+++ backend/weather_api/urls.py
@@ -8,9 +8,10 @@ from .views import (
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    # 1. Auth: Sign in & Log in 
+    # 1. Sign in & Log in 
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),       
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    #2. Search city weather
     path('search-city/', SearchCityView.as_view(), name='search-city'), 
 ]
